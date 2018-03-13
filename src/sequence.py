47baca8e4deac27c5903d6ead4780f8107579589
Sahand <aksahand@gmail.com>
2018-03-13 14:59:44 -0400

    updating sequence.py

--- src/sequence.py
+++ src/sequence.py
@@ -6,18 +6,31 @@ class Sequence:
 	def __init__(self):
 		self.log = Log()
 
-	def create_k_grams(self , n = 5):
+	def create_k_grams(self , k = 5):
+		kgram_set = set()
 		kgram_count = {}
+		s = 0
 		user_seq = self.log.get_user_seq()
 		for user in user_seq:
-			fivegrams = ngrams(user_seq[user] , n)
+			kgrams = ngrams(user_seq[user] , k)
 			l = []
-			for grams in fivegrams:
-				l.append(grams)
+			for gram in kgrams:
+				if gram[0] == 'r' or gram[0] == 's' or gram[0] == 'j':
+					kgram_set.add(gram)
+					l.append(gram)
+
+
 			kgram_count[user] = Counter(l)
-		print kgram_count['2']
+
+		return  kgram_set , kgram_count
+
+
+
+
+
+
 
 
 
 s = Sequence()
-s.create_k_grams()
+print s.create_k_grams()
